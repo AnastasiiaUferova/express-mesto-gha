d77bd9b93bdbbfb52c5117de4657f94a840a861b
‘Anastasiia <miss.iuferova@yandex.ru>
2022-04-16 18:01:53 +0200

    13 sprint

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 app.use(userRoutes);
 app.use(cardRoutes);
 app.use((req,res) => {
-  res.status(400).send({message: "Ресурс не найден."})
+  res.status(404).send({message: "Ресурс не найден."})
 })
 
 
